75f699a95167dad4f8ed556f1be32c08caa855c9
schlaubi <michael@rittmeister.in>
2024-09-18 11:54:09 +0200

    Fix issues with sql schema

---
+++ schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS backend;
+CREATE DATABASE backend;
+
+DROP TABLE IF EXISTS readings;
+DROP TABLE IF EXISTS customers;
+
+DROP TYPE IF EXISTS Gender;
+CREATE TYPE Gender AS ENUM ('M', 'W', 'D', 'U');
+
+CREATE TABLE customers
+(
+    id         SERIAL PRIMARY KEY,
+    last_name  TEXT,
+    first_name TEXT,
+    birth_date TIMESTAMP,
+    gender     Gender
+);
+
+DROP TYPE IF EXISTS MeterType;
+CREATE TYPE MeterType AS ENUM ('HEIZUNG','STROM','WASSER','UNBEKANNT');
+
+CREATE TABLE readings
+(
+    id          SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers (id),
+    read_date   TIMESTAMP,
+    meter_id    INT,
+    meter_type  MeterType,
+    meter_count INT,
+    substitute  BOOLEAN,
+    comment     TEXT
+);
