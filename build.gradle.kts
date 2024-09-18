33aa7ad5e69268efa60a2c86632e434c7882010d
schlaubi <michael@rittmeister.in>
2024-09-18 13:51:13 +0200

    Revert "deleted gradle files"
    This reverts commit 6ac26f7dc473bbf5ed0bb234633be226c62d88e6.

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
+
+application {
+    applicationDefaultJvmArgs = listOf("--enable-preview")
+    mainClass = "Main"
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(22)
+    }
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.compilerArgs.add("--enable-preview")
+        options.encoding = "UTF-8"
+    }
+}
