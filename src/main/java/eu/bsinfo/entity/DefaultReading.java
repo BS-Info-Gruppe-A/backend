22f1242304987caeb2cbd52fdc6afbce50ad885b
Jakob Braun <jakobbraun@MBP-von-Jakob.mschool-ad.muenchen.musin.de>
2024-10-14 10:45:59 +0200

    added default reading and IReading Interface

---
+++ src/main/java/eu/bsinfo/entity/DefaultReading.java
@@ -0,0 +1,121 @@
+package eu.bsinfo.entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+public class DefaultReading implements IReading{
+
+    private String comment;
+    private ICustomer customer;
+    private LocalDate dateOfReading;
+    private KindOfMeter kindOfMeter;
+    private Double meterCount;
+    private String meterId;
+    private Boolean substitute;
+    private String printDateOfReading;
+
+    public DefaultReading(String comment, ICustomer customer, LocalDate dateOfReading, KindOfMeter kindOfMeter, Double meterCount, String meterId, Boolean substitute, String printDateOfReading) {
+        this.comment = comment;
+        this.customer = customer;
+        this.dateOfReading = dateOfReading;
+        this.kindOfMeter = kindOfMeter;
+        this.meterCount = meterCount;
+        this.meterId = meterId;
+        this.substitute = substitute;
+        this.printDateOfReading = printDateOfReading;
+    }
+
+    @Override
+    public String getComment(){
+        return comment;
+        }
+
+    @Override
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public ICustomer getCustomer() {
+        return customer;
+    }
+
+    @Override
+    public void setCustomer(ICustomer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public LocalDate getDateOfReading() {
+        return dateOfReading;
+    }
+
+    @Override
+    public void setDateOfReading(LocalDate dateOfReading) {
+        this.dateOfReading = dateOfReading;
+    }
+
+    @Override
+    public KindOfMeter getKindOfMeter() {
+        return kindOfMeter;
+    }
+
+    @Override
+    public void setKindOfMeter(KindOfMeter kindOfMeter) {
+        this.kindOfMeter = kindOfMeter;
+    }
+
+    @Override
+    public Double getMeterCount() {
+        return meterCount;
+    }
+
+    @Override
+    public void setMeterCount(Double meterCount) {
+        this.meterCount = meterCount;
+    }
+
+    @Override
+    public String getMeterId() {
+        return meterId;
+    }
+
+    @Override
+    public void setMeterId(String meterId) {
+        this.meterId = meterId;
+    }
+
+    @Override
+    public Boolean getSubstitute() {
+        return substitute;
+    }
+
+    @Override
+    public String printDateOfReading() {
+        return "";
+    }
+
+    @Override
+    public void setSubstitute(Boolean substitute) {
+        this.substitute = substitute;
+    }
+
+    public String getPrintDateOfReading() {
+        return printDateOfReading;
+    }
+
+    public void setPrintDateOfReading(String printDateOfReading) {
+        this.printDateOfReading = printDateOfReading;
+    }
+
+    @Override
+    public UUID getId() {
+        return null;
+    }
+
+    @Override
+    public void setId(UUID id) {
+
+    }
+}
--- src/main/java/eu/bsinfo/entity/IReading.java
+++ src/main/java/eu/bsinfo/entity/IReading.java
@@ -1,41 +1,66 @@
 package eu.bsinfo.entity;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.UUID;
 
 public interface IReading extends IId {
 
-   enum KindOfMeter {
-      HEIZUNG, STROM, UNBEKANNT, WASSER;
-   }
+    enum KindOfMeter {
+        HEIZUNG, STROM, UNBEKANNT, WASSER;
+    }
 
-   String getComment();
+    String getComment();
 
-   ICustomer getCustomer();
+    ICustomer getCustomer();
 
-   LocalDate getDateOfReading();
+    LocalDate getDateOfReading();
 
-   KindOfMeter getKindOfMeter();
+    KindOfMeter getKindOfMeter();
 
-   Double getMeterCount();
+    Double getMeterCount();
 
-   String getMeterId();
+    String getMeterId();
 
-   Boolean getSubstitute();
+    Boolean getSubstitute();
 
-   String printDateOfReading();
+    String printDateOfReading();
 
-   void setComment(String comment);
+    void setComment(String comment);
 
-   void setCustomer(ICustomer customer);
+    void setCustomer(ICustomer customer);
 
-   void setDateOfReading(LocalDate dateOfReading);
+    void setDateOfReading(LocalDate dateOfReading);
 
-   void setKindOfMeter(KindOfMeter kindOfMeter);
+    void setKindOfMeter(KindOfMeter kindOfMeter);
 
-   void setMeterCount(Double meterCount);
+    void setMeterCount(Double meterCount);
 
-   void setMeterId(String meterId);
+    void setMeterId(String meterId);
 
-   void setSubstitute(Boolean substitute);
+    void setSubstitute(Boolean substitute);
 
+
+    /**
+     * Creates a new {@link IReading} from a {@link ResultSet}
+     *
+     * @param resultSet the {@link ResultSet} containing the data
+     * @return the newly created readings
+     * @throws SQLException if an SQL error occurs
+     */
+    static IReading from(ResultSet resultSet) throws SQLException {
+        var comment = resultSet.getString("comment");
+        var customer = resultSet.getObject("customer");
+        var dateOfReading = resultSet.getObject("dateOfReading");
+        var kindOfMeter = resultSet.getObject("kindOfMeter");
+        var meterCount = resultSet.getDouble("meterCount");
+        var meterId = resultSet.getString("meterId");
+        var substitute = resultSet.getBoolean("substitute");
+        var printDateOfReading = resultSet.getString("printDateOfReading");
+
+        //*must be implemented
+        //return new DefaultReading(comment, customer, dateOfReading, kindOfMeter, meterCount, meterId, substitute, printDateOfReading);
+        return null;
+    }
 }
