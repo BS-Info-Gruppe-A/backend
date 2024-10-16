5aca42b224d9e4d2f05fc0122d28f014143eada2
Michael Rittmeister <michael@rittmeister.in>
2024-10-16 12:00:08 +0200

    Repository cleanup - Split queries into multiple lines to improve readability - use single try-catch block

--- src/main/java/eu/bsinfo/database/repository/CustomerRepository.java
+++ src/main/java/eu/bsinfo/database/repository/CustomerRepository.java
@@ -30,58 +30,59 @@ public class CustomerRepository extends Repository<ICustomer> {
     //CRUD Read funktion
     @Override
     public @Nullable ICustomer findById(@NotNull UUID id) throws SQLException {
-        try (var connection = databaseManager.getConnection()) {
-            try(var statement = connection.prepareStatement("SELECT * FROM customers WHERE id = ?")) {
-                statement.setObject(1, id);
-                try(var resultSet = statement.executeQuery()) {
-                    if (resultSet.next()) {
-                        return ICustomer.from(resultSet);
-                    } else {
-                        return null;
-                    }
+        try (var connection = databaseManager.getConnection();
+             var statement = connection.prepareStatement("SELECT * FROM customers WHERE id = ?")) {
+            statement.setObject(1, id);
+            try (var resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return ICustomer.from(resultSet);
+                } else {
+                    return null;
                 }
             }
+
         }
     }
 
     //CRUD Delete funktion
     @Override
     public boolean delete(@NotNull UUID id) throws SQLException {
-        try (var connection = databaseManager.getConnection()) {
-            try (var statement = connection.prepareStatement("DELETE FROM customers WHERE id = ?")) {
-                statement.setObject(1, id);
-                return statement.executeUpdate() > 0;
-            }
+        try (var connection = databaseManager.getConnection();
+             var statement = connection.prepareStatement("DELETE FROM customers WHERE id = ?")) {
+            statement.setObject(1, id);
+            return statement.executeUpdate() > 0;
         }
     }
 
     //CRUD Create funktion
     @Override
     public boolean insert(@NotNull ICustomer entity) throws SQLException {
-        try (var connection = databaseManager.getConnection()) {
-            try (var statement = connection.prepareStatement("INSERT INTO customers (id, birth_date, first_name, gender, last_name) VALUES (?, ?, ?, ?, ?)")) {
-                statement.setObject(1, entity.getId());
-                statement.setTimestamp(2, Timestamp.valueOf(entity.getBirthDate().atTime(0, 0)));
-                statement.setString(3, entity.getFirstName());
-                statement.setObject(4, entity.getGender(), Types.OTHER);
-                statement.setString(5, entity.getLastName());
-                return statement.executeUpdate() > 0;
-            }
+        try (var connection = databaseManager.getConnection();
+             var statement = connection.prepareStatement("""
+                     INSERT INTO customers(id, birth_date, first_name, gender, last_name) VALUES (?, ?, ?, ?, ?)""")) {
+            statement.setObject(1, entity.getId());
+            statement.setTimestamp(2, Timestamp.valueOf(entity.getBirthDate().atTime(0, 0)));
+            statement.setString(3, entity.getFirstName());
+            statement.setObject(4, entity.getGender(), Types.OTHER);
+            statement.setString(5, entity.getLastName());
+            return statement.executeUpdate() > 0;
+
         }
     }
 
     //CRUD Update funktion
     @Override
     public boolean update(@NotNull ICustomer entity) throws SQLException {
-        try (var connection = databaseManager.getConnection()) {
-            try (var statement = connection.prepareStatement("UPDATE customers SET birth_date = ?, first_name = ?, gender = ?, last_name = ? WHERE id = ?")) {
-                statement.setTimestamp(1, Timestamp.valueOf(entity.getBirthDate().atTime(0, 0)));
-                statement.setString(2, entity.getFirstName());
-                statement.setObject(3, entity.getGender(), Types.OTHER);
-                statement.setString(4, entity.getLastName());
-                statement.setObject(5, entity.getId());
-                return statement.executeUpdate() > 0;
-            }
+        try (var connection = databaseManager.getConnection();
+             var statement = connection.prepareStatement("""
+                     UPDATE customers SET birth_date = ?,\
+                      first_name = ?, gender = ?, last_name = ? WHERE id = ?""")) {
+            statement.setTimestamp(1, Timestamp.valueOf(entity.getBirthDate().atTime(0, 0)));
+            statement.setString(2, entity.getFirstName());
+            statement.setObject(3, entity.getGender(), Types.OTHER);
+            statement.setString(4, entity.getLastName());
+            statement.setObject(5, entity.getId());
+            return statement.executeUpdate() > 0;
         }
     }
 }
--- src/main/java/eu/bsinfo/database/repository/ReadingRepository.java
+++ src/main/java/eu/bsinfo/database/repository/ReadingRepository.java
@@ -27,19 +27,18 @@ public class ReadingRepository extends Repository<IReading> {
 
     @Override
     public @Nullable IReading findById(@NotNull UUID id) throws SQLException {
-        try (var connection = databaseManager.getConnection()) {
-            try(var statement = connection.prepareStatement("""
-                    SELECT * FROM readings
-                    JOIN customers c on c.id = customer_id
-                    WHERE readings.id = ?
-                    """)) {
-                statement.setObject(1, id);
-                try(var resultSet = statement.executeQuery()) {
-                    if (resultSet.next()) {
-                        return IReading.from(resultSet);
-                    } else {
-                        return null;
-                    }
+        try (var connection = databaseManager.getConnection();
+             var statement = connection.prepareStatement("""
+                     SELECT * FROM readings
+                     JOIN customers c on c.id = customer_id
+                     WHERE readings.id = ?
+                     """)) {
+            statement.setObject(1, id);
+            try (var resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return IReading.from(resultSet);
+                } else {
+                    return null;
                 }
             }
         }
@@ -47,46 +46,47 @@ public class ReadingRepository extends Repository<IReading> {
 
     @Override
     public boolean delete(@NotNull UUID id) throws SQLException {
-        try (var connection = databaseManager.getConnection()) {
-            try (var statement = connection.prepareStatement("DELETE FROM readings WHERE id = ?")) {
-                statement.setObject(1, id);
-                return statement.executeUpdate() > 0;
-            }
+        try (var connection = databaseManager.getConnection();
+             var statement = connection.prepareStatement("DELETE FROM readings WHERE id = ?")) {
+            statement.setObject(1, id);
+            return statement.executeUpdate() > 0;
+
         }
     }
 
     @Override
     public boolean insert(@NotNull IReading entity) throws SQLException {
-        try (var connection = databaseManager.getConnection()) {
-            try (var statement = connection.prepareStatement("INSERT INTO readings (id, comment, customer_id, read_date, meter_type, meter_count, meter_id, substitute) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")) {
-                statement.setObject(1, entity.getId());
-                statement.setString(2, entity.getComment());
-                statement.setObject(3, entity.getCustomer().getId());
-                statement.setTimestamp(4, Timestamp.valueOf(entity.getDateOfReading().atTime(0, 0)));
-                statement.setObject(5, entity.getKindOfMeter(), Types.OTHER);
-                statement.setDouble(6, entity.getMeterCount());
-                statement.setInt(7, entity.getMeterId());
-                statement.setBoolean(8, entity.getSubstitute());
-                return statement.executeUpdate() > 0;
-            }
+        try (var connection = databaseManager.getConnection();
+             var statement = connection.prepareStatement("""
+                     INSERT INTO readings(id, comment, customer_id, read_date, meter_type, meter_count, meter_id, substitute)
+                     VALUES (?, ?, ?, ?, ?, ?, ?, ?)""")) {
+            statement.setObject(1, entity.getId());
+            statement.setString(2, entity.getComment());
+            statement.setObject(3, entity.getCustomer().getId());
+            statement.setTimestamp(4, Timestamp.valueOf(entity.getDateOfReading().atTime(0, 0)));
+            statement.setObject(5, entity.getKindOfMeter(), Types.OTHER);
+            statement.setDouble(6, entity.getMeterCount());
+            statement.setInt(7, entity.getMeterId());
+            statement.setBoolean(8, entity.getSubstitute());
+            return statement.executeUpdate() > 0;
         }
     }
 
     @Override
     public boolean update(@NotNull IReading entity) throws SQLException {
-        try (var connection = databaseManager.getConnection()) {
-            try (var statement = connection.prepareStatement("UPDATE readings SET comment = ?, customer_id = ?, read_date = ?, meter_type = ?,  meter_count = ?,  meter_id = ?, substitute = ? WHERE id = ?")) {
-                statement.setString(1, entity.getComment());
-                statement.setObject(2, entity.getCustomer().getId());
-                statement.setTimestamp(3, Timestamp.valueOf(entity.getDateOfReading().atTime(0, 0)));
-                statement.setObject(4, entity.getKindOfMeter(), Types.OTHER);
-                statement.setDouble(5, entity.getMeterCount());
-                statement.setInt(6, entity.getMeterId());
-                statement.setBoolean(7, entity.getSubstitute());
-                statement.setObject(8,entity.getId());
-                return statement.executeUpdate() > 0;
-            }
+        try (var connection = databaseManager.getConnection();
+             var statement = connection.prepareStatement("""
+                     UPDATE readings SET comment = ?, customer_id = ?, read_date = ?, meter_type = ?,  meter_count = ?,
+                     meter_id = ?, substitute = ? WHERE id = ?""")) {
+            statement.setString(1, entity.getComment());
+            statement.setObject(2, entity.getCustomer().getId());
+            statement.setTimestamp(3, Timestamp.valueOf(entity.getDateOfReading().atTime(0, 0)));
+            statement.setObject(4, entity.getKindOfMeter(), Types.OTHER);
+            statement.setDouble(5, entity.getMeterCount());
+            statement.setInt(6, entity.getMeterId());
+            statement.setBoolean(7, entity.getSubstitute());
+            statement.setObject(8, entity.getId());
+            return statement.executeUpdate() > 0;
         }
     }
-
 }
