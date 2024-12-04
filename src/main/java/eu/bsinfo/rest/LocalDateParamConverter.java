3ad430b23cc1a2ff10da4f331a993cbfa564cca9
Jakob Braun <jakobbraun@MacBookPro.mschool-ad.muenchen.musin.de>
2024-12-04 12:12:23 +0100

    adding converter for LocalDate

---
+++ src/main/java/eu/bsinfo/rest/LocalDateParamConverter.java
@@ -0,0 +1,36 @@
+package eu.bsinfo.rest;
+
+import jakarta.ws.rs.ext.ParamConverter;
+import jakarta.ws.rs.ext.ParamConverterProvider;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateParamConverter implements ParamConverter<LocalDate> {
+    private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    @Override
+    public LocalDate fromString(String s) {
+        return LocalDate.from(format.parse(s));
+    }
+
+    @Override
+    public String toString(LocalDate date) {
+        return format.format(date);
+    }
+
+    @jakarta.ws.rs.ext.Provider
+    public static class Provider implements ParamConverterProvider {
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> ParamConverter<T> getConverter(Class<T> aClass, Type type, Annotation[] annotations) {
+            if (aClass.equals(LocalDate.class)) {
+                return (ParamConverter<T>) new LocalDateParamConverter();
+            }
+
+            return null;
+        }
+    }
+}
