369a5d39fa0c0ebb44b0b522e9592388898206a7
Michael Rittmeister <michael@rittmeister.in>
2024-12-06 16:59:33 +0100

    Add CORS support

---
+++ src/main/java/eu/bsinfo/rest/CORSHandler.java
@@ -0,0 +1,19 @@
+package eu.bsinfo.rest;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class CORSHandler implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", System.getenv("CORS_ORIGIN"));
+        headers.add("Access-Control-Allow-Headers", "Accept, Content-Type");
+        headers.add("Access-Control-Allow-Credentials", "true");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    }
+}
